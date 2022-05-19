90d3726e15375474d3f03f5c86144477423fef63
Matthew Ronchetto <hello@doamatto.xyz>
2022-05-19 09:12:43 -0700

    fix: wrong variable

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#modprobe xt_mark
+modprobe xt_mark
 echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
@@ -17,7 +17,7 @@ if [ ! -S /var/run/tailscale/tailscaled.sock ]; then
     exit 1
 fi
 
-until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=fly-${FLY_REGION} --advertise-exit-node
+until /app/tailscale up --authkey=${TAILSCALE_AUTH} --hostname=fly-${FLY_REGION} --advertise-exit-node
 do
     sleep 0.1
 done
